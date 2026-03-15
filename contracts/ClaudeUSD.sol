29adeb0b5cdc82dd811705b5d810764c277c9ae5
ZENO <saintwilight7@gmail.com>
2026-03-15 01:45:01 -0400

    add approve and transferFrom - full ERC-20 ClaudeUSD

--- contracts/ClaudeUSD.sol
+++ contracts/ClaudeUSD.sol
@@ -7,14 +7,27 @@ contract ClaudeUSD {
     uint256 public totalSupply = 9999;
 
     mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) public allowances;
 
     constructor() {
         balances[msg.sender] = totalSupply;
     }
 
-    function transfer(address to, uint256 amount) public{
+    function transfer(address to, uint256 amount) public {
         require(balances[msg.sender] >= amount, "Insufficient balance");
         balances[msg.sender] -=amount;
+        balances[to] += amount;
+    }
+
+    function approve(address spender, uint256 amount) public {
+        allowances[msg.sender][spender] = amount;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public {
+        require (balances[from] >= amount, "Insufficient balance");
+        require(allowances[from][msg.sender] >= amount, "Not approved");
+        allowances[from][msg.sender] -= amount;
+        balances[from] -= amount;
         balances[to] += amount;
     }
 }
